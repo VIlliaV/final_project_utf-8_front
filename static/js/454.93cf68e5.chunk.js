8c649f105ebffb77917ff89735238b0227d800e8
VIlliaV <110853267+VIlliaV@users.noreply.github.com>
2023-07-08 12:31:41 +0000

    Deploying to gh-pages from @ VIlliaV/final_project_utf-8_front@bf1c515dbf9f31cc60a807b191f7486a3d7a5c2a 🚀

---
+++ static/js/454.93cf68e5.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project_utf_8_front=self.webpackChunkfinal_project_utf_8_front||[]).push([[454],{4454:function(t,f,n){n.r(f);var u=n(2634),e=n(3329);f.default=function(){return(0,e.jsx)(u.g,{})}}}]);
+//# sourceMappingURL=454.93cf68e5.chunk.js.map
