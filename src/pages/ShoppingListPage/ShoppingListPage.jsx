923603bbe86d8d9c9534f3554f305c0ad24a11e3
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-07-08 20:15:05 +0300

    UPD: markup mobile&tablet

--- src/pages/ShoppingListPage/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage/ShoppingListPage.jsx
@@ -1,19 +1,25 @@
 // ShoppingListPage маршрут /shopping-list
-// "Зверстати сторінку (мобільна, планшет та десктопна версії)
 
+// ShoppingListPage
+// + Зверстати сторінку (мобільна, планшет та десктопна версії)
 // Компонент рендерить:
 //  - MainPageTitle - універсальний компонент заголовка сторінки.
 //  - IngredientsShoppingList
-// Компонент виконує запит за колекцією інгрідієнтів зі списку покупок, якщо вона відсутня в store."  ShoppingListPage
-// "Компонент підписаний на колекцію інгрідієнтів зі списку покупок в store
+// Компонент виконує запит за колекцією інгрідієнтів зі списку покупок, якщо вона відсутня в store.
+// Тобто перевірити store, якщо там немає - запит у список покупок.
+
+// IngredientsShoppingList
+// Компонент рендерить:
+// Компонент підписаний на колекцію інгрідієнтів зі списку покупок в store
 // Компонент рендерить список елементів з інгрідієнтами до покупки та заголовок до цього списку.
 // Кожен елемент списку має:
 //  - зображення інгрідієнту або плейсхолдер
 //  - к-сть в відповідних одиницях виміру
 //  - кнопку для видалення елементу зі списку
-// Клік по кнопці видалення повинен видалити елемент на бекенді, та його ж в списку на сторінці." IngredientsShoppingList
-import React from 'react';
+// Клік по кнопці видалення повинен видалити елемент на бекенді, та його ж в списку на сторінці.
+
 import {
+  SLContainer,
   SLHeader,
   SLHeaderText,
   SLItem,
@@ -21,8 +27,9 @@ import {
   SLItemImage,
   SLItemImageName,
   SLItemRemove,
-  SLList,
+  IngredientsShoppingList,
   SLMainTitle,
+  MainPageTitle,
 } from './ShoppingListPage.styled';
 
 // const liShoppingListTitle = styled.li`
@@ -31,14 +38,17 @@ import {
 
 function ShoppingListPage() {
   return (
-    <>
-      <SLMainTitle>Shopping list</SLMainTitle>
-      <SLHeader>
-        <SLHeaderText>Products</SLHeaderText>
-        <SLHeaderText>Amount</SLHeaderText>
-        <SLHeaderText>Remove</SLHeaderText>
-      </SLHeader>
-      <SLList>
+    <SLContainer>
+      <MainPageTitle>
+        <SLMainTitle>Shopping list</SLMainTitle>
+        <SLHeader>
+          <SLHeaderText>Products</SLHeaderText>
+          <SLHeaderText>Amount</SLHeaderText>
+          <SLHeaderText>Remove</SLHeaderText>
+        </SLHeader>
+      </MainPageTitle>
+
+      <IngredientsShoppingList>
         <SLItem>
           <SLItemImage class="cards__animation">
             <picture>
@@ -77,8 +87,47 @@ function ShoppingListPage() {
             </svg>
           </SLItemRemove>
         </SLItem>
-      </SLList>
-    </>
+
+        <SLItem>
+          <SLItemImage class="cards__animation">
+            <picture>
+              <source
+                srcset="
+                        ./images/desktop/portfolio2-1x.jpg 1x,
+                        ./images/desktop/portfolio2-2x.jpg 2x
+                      "
+                media="(min-width: 1200px)"
+              />
+              <source
+                srcset="
+                        ./images/tablet/portfolio2-1x.jpg 1x,
+                        ./images/tablet/portfolio2-2x.jpg 2x
+                      "
+                media="(min-width: 768px)"
+              />
+              <source
+                srcset="
+                        ./images/mobile/portfolio2-1x.jpg 1x,
+                        ./images/mobile/portfolio2-2x.jpg 2x
+                      "
+                media="(max-width: 767px)"
+              />
+              <img src="source-name-here" alt="goods-imgName" />
+            </picture>
+          </SLItemImage>
+
+          <SLItemImageName>Item name</SLItemImageName>
+
+          <SlItemAmount>400g</SlItemAmount>
+
+          <SLItemRemove type="button" data-menu-close="" class="menu-close menu-close--menu">
+            <svg class="menu-close__icon">
+              <use href="/Ice-Cream/icons.c1fa56c8.svg#menu-close"></use>
+            </svg>
+          </SLItemRemove>
+        </SLItem>
+      </IngredientsShoppingList>
+    </SLContainer>
   );
 }
 
--- src/pages/ShoppingListPage/ShoppingListPage.styled.js
+++ src/pages/ShoppingListPage/ShoppingListPage.styled.js
@@ -4,120 +4,261 @@ import styled from 'styled-components';
 // font-family: 'Montserrat', sans-serif;
 // font-family: 'Poppins', sans-serif;
 
+// position: relative;
+// // margin-bottom: 38px;
+// @media screen and (min-width: $tablet) {
+//   // margin-bottom: 40px;
+// }
+// @media screen and (min-width: $desktop) {
+//   // margin-bottom: 120px;
+// }
+const mobile = '375px';
+const tablet = '768px';
+const desktop = '1440px';
+
+const SLContainer = styled.div`
+  max-width: 375px;
+  margin: 0 auto;
+
+  padding-left: 8px;
+  padding-right: 8px;
+
+  @media screen and (min-width: ${tablet}) {
+    min-width: 768px;
+    padding-left: 32px;
+    padding-right: 32px;
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    min-width: 1440px;
+    padding-left: 100px;
+    padding-right: 100px;
+  }
+`;
+
+const MainPageTitle = styled.div``;
+
 const SLMainTitle = styled.h2`
-  margin-top: 112px;
+  margin-left: 8px;
 
   color: #001833;
   font-family: 'Poppins', sans-serif;
-  font-size: 44px;
+  font-size: 28px;
   font-style: normal;
   font-weight: 600;
-  line-height: 44px;
-  letter-spacing: -0.88px;
+  line-height: 28px;
+  letter-spacing: -0.56px;
+
+  @media screen and (min-width: ${tablet}) {
+    font-size: 32px;
+    line-height: 32px;
+    letter-spacing: -0.64px;
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    margin-top: 112px;
+    font-size: 44px;
+    line-height: 44px;
+    letter-spacing: -0.88px;
+  }
 `;
 
 const SLHeader = styled.div`
-  width: 1240px;
-  height: 60px;
+  max-width: 359px;
+  height: 38px;
 
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding: 10px;
+  margin-top: 50px;
 
-  padding: 16px 40px;
-  margin-top: 72px;
-  padding-top: 20px;
-  padding-bottom: 20px;
-  padding-left: 40px;
-  padding-right: 40px;
-
+  border-radius: 8px;
   background-color: #8baa36;
 
   *:nth-last-child(2) {
     margin-left: auto;
-    margin-right: 142px;
+    margin-right: 24px;
+  }
+
+  @media screen and (min-width: ${tablet}) {
+    min-width: 704px;
+    height: 58px;
+
+    padding: 20px;
+    margin-top: 72px;
+
+    *:nth-last-child(2) {
+      margin-left: auto;
+      margin-right: 78px;
+    }
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    min-width: 1240px;
+    height: 60px;
+
+    padding: 20px 40px;
+
+    *:nth-last-child(2) {
+      margin-left: auto;
+      margin-right: 142px;
+    }
   }
 `;
 
 const SLHeaderText = styled.p`
-  display: flex;
-
   color: #fafafa;
   font-family: 'Poppins', sans-serif;
-  font-size: 18px;
+  font-size: 12px;
   font-style: normal;
   font-weight: 600;
-  line-height: 26px;
+  line-height: 18px;
+
+  @media screen and (min-width: ${tablet}) {
+    font-size: 18px;
+    line-height: 26px;
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    font-size: 18px;
+    line-height: 26px;
+  }
 `;
 
-const SLList = styled.ul`
-  margin-top: 50px;
-  margin-left: 40px;
-  margin-right: 40px;
-  margin-bottom: 238px;
-
+const IngredientsShoppingList = styled.ul`
   display: flex;
   flex-direction: column;
-  gap: 88px;
+  gap: 48px;
+  margin-top: 32px;
+  margin-left: 8px;
+  margin-right: 8px;
+  margin-bottom: 100px;
+
+  @media screen and (min-width: ${tablet}) {
+    gap: 88px;
+    margin-top: 50px;
+    margin-left: 0;
+    margin-right: 0;
+    margin-bottom: 200px;
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    margin-left: 40px;
+    margin-right: 40px;
+    margin-bottom: 196px;
+  }
 `;
 
 const SLItem = styled.li`
+  position: relative;
   display: flex;
+
+  ::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -24px;
+
+    width: 100%;
+    height: 1px;
+    background-color: #e0e0e0;
+  }
+
+  @media screen and (min-width: ${tablet}) {
+    width: 704px;
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    width: 1160px;
+  }
 `;
 
 const SLItemImage = styled.div`
-  /* display: flex; */
-  /* justify-content: center; */
-  /* align-items: center; */
-  width: 93px;
-  height: 97px;
-  padding: 8px 6px;
+  width: 60px;
+  height: 60px;
+  padding: 6px;
 
-  border-radius: 8px;
+  border-radius: 6px;
   background: #ebf3d4;
+
+  @media screen and (min-width: ${tablet}) {
+    width: 93px;
+    height: 97px;
+    padding: 8px 6px;
+    border-radius: 8px;
+  }
 `;
 const SLItemImageName = styled.p`
-  width: 140px;
-  height: 97px;
-
-  margin-left: 16px;
-
-  align-items: flex-start;
+  margin-left: 10px;
 
   color: #3e4462;
   font-family: 'Poppins', sans-serif;
-  font-size: 16px;
+  font-size: 10px;
   font-style: normal;
   font-weight: 500;
-  line-height: 24px;
-`;
-const SlItemAmount = styled.p`
-  width: 68px;
-  height: 35px;
+  line-height: 12px;
 
-  margin-left: 620px;
-  padding: 4px 8px;
+  @media screen and (min-width: ${tablet}) {
+    margin-left: 16px;
+
+    font-size: 16px;
+    line-height: 24px;
+  }
+`;
+
+const SlItemAmount = styled.p`
+  min-width: 38px;
+  height: 23px;
 
   display: flex;
   align-items: center;
   justify-content: center;
+  margin-left: auto;
+  margin-right: 46px;
+  padding: 4px;
 
   border-radius: 4px;
   background: #8baa36;
 
   color: #fafafa;
+  text-align: center;
   font-family: 'Poppins', sans-serif;
-  font-size: 18px;
+  font-size: 10px;
   font-style: normal;
   font-weight: 600;
   line-height: normal;
-`;
-const SLItemRemove = styled.button`
-  width: 20px;
-  height: 20px;
 
-  margin-left: 174px;
-  margin-right: 28px;
+  @media screen and (min-width: ${tablet}) {
+    min-width: 68px;
+    height: 35px;
+
+    margin-right: 110px;
+    padding: 4px 8px;
+
+    font-size: 18px;
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    margin-right: 174px;
+  }
+`;
+
+const SLItemRemove = styled.button`
+  width: 14px;
+  height: 14px;
+
+  margin-right: 18px;
+
+  @media screen and (min-width: ${tablet}) {
+    width: 20px;
+    height: 20px;
+    margin-right: 44px;
+  }
+
+  @media screen and (min-width: ${desktop}) {
+    margin-right: 28px;
+  }
 
   /* <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
   <path d="M15.625 4.375L4.375 15.625" stroke="#333333" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
@@ -125,4 +266,16 @@ const SLItemRemove = styled.button`
   </svg> */
 `;
 
-export { SLMainTitle, SLHeader, SLHeaderText, SLList, SLItem, SLItemRemove, SlItemAmount, SLItemImageName, SLItemImage };
+export {
+  SLMainTitle,
+  SLHeader,
+  SLHeaderText,
+  IngredientsShoppingList,
+  SLItem,
+  SLItemRemove,
+  SlItemAmount,
+  SLItemImageName,
+  SLItemImage,
+  SLContainer,
+  MainPageTitle,
+};
