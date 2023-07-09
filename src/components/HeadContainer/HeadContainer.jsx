bc46437eeef0b3fe729854a560a281de85a891bb
Illia <illia.vasiliev@gmail.com>
2023-07-09 15:37:56 +0300

    add children to container

--- src/components/HeadContainer/HeadContainer.jsx
+++ src/components/HeadContainer/HeadContainer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Container } from './HeadContainer.styled';
 
-function HeadContainer() {
-  return <Container></Container>;
+function HeadContainer({ children }) {
+  return <Container>{children}</Container>;
 }
 
 export default HeadContainer;
