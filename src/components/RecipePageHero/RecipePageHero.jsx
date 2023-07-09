85621813c67cd89ec93ae8542d97ba065ef8413f
Iryna Popova <popovaira969@gmail.com>
2023-07-09 21:59:49 +0300

    CREATED: RecipePageHero and RecipeInngredientsList

--- src/components/RecipePageHero/RecipePageHero.jsx
+++ src/components/RecipePageHero/RecipePageHero.jsx
@@ -1,6 +1,19 @@
-console.log("It's my part RecipePageHero");
-const RecipePageHero = () => {
-  
+import React from 'react';
+import { TitleRecipe, Description, Button, Time } from './RecipePageHero.styled';
+
+const RecipePageHero = ({ title, description, time }) => {
+  return (
+    <>
+      <TitleRecipe>{title}Salmon Avocado Salad</TitleRecipe>
+      <Description>
+        {description}
+        Is a healthy salad recipe that’s big on nutrients and flavor. A moist, pan seared salmon is layered on top of spinach, avocado,
+        tomatoes, and red onions. Then drizzled with a homemade lemon vinaigrette.
+      </Description>
+      <Button>Add to favorite recipes</Button>
+      <Time>{time} min</Time>
+    </>
+  );
 };
 
 export default RecipePageHero;
