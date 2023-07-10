596368dded84d8121fa30bd04eb9232b70420a0d
Dmitry Yavtushenko <rosengan22@gmail.com>
2023-07-11 00:28:15 +0300

    added responsive styles

--- src/components/CategoriesRecipesList/CategoriesRecipesList.jsx
+++ src/components/CategoriesRecipesList/CategoriesRecipesList.jsx
@@ -1,5 +1,5 @@
 import { CategoryRecipeCard } from '../CategoryRecipeCard/CategoryRecipeCard';
-import { CategoriesRecipesContainer } from './CategoriesRecipesList.styled';
+import { CategoriesRecipesContainer, CategoryRecipeCardWrapper } from './CategoriesRecipesList.styled';
 import { useParams } from 'react-router-dom';
 
 const recipes = require('../../back/recipes.json');
@@ -7,15 +7,25 @@ const recipes = require('../../back/recipes.json');
 // console.log(recipes);
 
 export const CategoriesRecipesList = () => {
-  const currentCategory = useParams().categoryName.charAt(0).toUpperCase() + useParams().categoryName.slice(1);
-  //   console.log(currentCategory);
-  const filteredRecipes = recipes.filter(el => el.category === currentCategory);
-  //   console.log(filteredRecipes);
-  return (
-    <CategoriesRecipesContainer>
-      {filteredRecipes.map(({ _id: { $oid: id }, title, thumb }) => {
-        return <CategoryRecipeCard key={id} itemId={id} imageUrl={thumb} imageAlt={title} title={title}></CategoryRecipeCard>;
-      })}
-    </CategoriesRecipesContainer>
-  );
+    const currentCategory = useParams().categoryName.charAt(0).toUpperCase() + useParams().categoryName.slice(1);
+    //   console.log(currentCategory);
+    const filteredRecipes = recipes.filter(el => el.category === currentCategory);
+    //   console.log(filteredRecipes);
+    return (
+        <CategoriesRecipesContainer>
+            {filteredRecipes.map(({ _id: { $oid: id }, title, thumb }) => {
+                return (
+                    <CategoryRecipeCardWrapper>
+                        <CategoryRecipeCard
+                            key={id}
+                            itemId={id}
+                            imageUrl={thumb}
+                            imageAlt={title}
+                            title={title}
+                        ></CategoryRecipeCard>
+                    </CategoryRecipeCardWrapper>
+                );
+            })}
+        </CategoriesRecipesContainer>
+    );
 };
--- src/components/HeadContainer/HeadContainer.styled.js
+++ src/components/HeadContainer/HeadContainer.styled.js
@@ -2,17 +2,18 @@ import styled from 'styled-components';
 import { media } from '../../utils/media';
 
 export const Container = styled.div`
-  margin: 0 auto;
-  @media ${media.mobile} {
-    max-width: 768px;
-    padding: 50px 16px 100px;
-  }
+    margin: 0 auto;
+    @media ${media.mobile} {
+        max-width: 768px;
+        padding: 50px 16px 100px;
+    }
 
-  @media ${media.tablet} {
-    max-width: 1440px;
-    padding: 72px 32px 200px;
-  }
-  @media ${media.desktop} {
-    padding: 102px 100px 200px;
-  }
+    @media ${media.tablet} {
+        max-width: 1440px;
+
+        padding: 72px 32px 200px;
+    }
+    @media ${media.desktop} {
+        padding: 102px 100px 200px;
+    }
 `;
