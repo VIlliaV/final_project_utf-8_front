27fe3630384e94a5f73e369358fde6161a850228
Serhii Sydorenko <sidorchik91@gmail.com>
2023-07-10 17:03:42 +0300

    finished with mobile layout of Auth components/pages

--- src/components/Auth/AuthLayout/AuthLayout.jsx
+++ src/components/Auth/AuthLayout/AuthLayout.jsx
@@ -1,22 +1,13 @@
-import { Container, Link, Image } from './AuthLayout.styled';
+import { Container, Image } from './AuthLayout.styled';
 import { AuthForm } from 'components/Auth/AuthForm/AuthForm';
-import { useNavigate } from 'react-router-dom';
 import authorizationPageImage from 'img/authorizationPageImage.svg';
-import { useLocation } from 'react-router-dom';
 
 export const AuthLayout = () => {
-    const navigate = useNavigate();
-    const { pathname } = useLocation();
 
-    const handleNavigate = () => {
-        pathname === '/register' ? navigate('/signin') : navigate('/register');
-    }
-    
-      return (
-        <Container>
-          <Image src={authorizationPageImage} />
-          <AuthForm />
-          <Link onClick={handleNavigate}>{pathname === '/register' ? 'Sign In': "Registration"}</Link>
-        </Container>
-      );
-}
+  return (
+    <Container>
+      <Image src={authorizationPageImage} />
+      <AuthForm />
+    </Container>
+  );
+};
