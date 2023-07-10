fa995f200e2ffd47e925c4f954457dabc09bde2d
OlenaHras <alena.hras@gmail.com>
2023-07-10 13:52:52 +0300

    added search recipes by ingredient

--- src/components/SearchedRecipesList/SearchedRecipesList.jsx
+++ src/components/SearchedRecipesList/SearchedRecipesList.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
-import { NoResultWrapper } from './SearchedRecipesList.styled';
+import { NoResultWrapper, ListContainer } from './SearchedRecipesList.styled';
 
 import noResultsImg from './no-results-img.webp';
 import { CategoryRecipeCard } from 'components/CategoryRecipeCard/CategoryRecipeCard';
 
 const recipes = require('../../back/recipes.json');
-// const ingredients = require('../../back/ingredients.json');
+const ingredients = require('../../back/ingredients.json');
 
 const SearchedRecipesList = () => {
   const [searchParams] = useSearchParams();
@@ -15,19 +15,23 @@ const SearchedRecipesList = () => {
   useEffect(() => {
     const value = searchParams.get('query');
     if (value) {
-      console.log(value);
       const result = recipes.filter(recipe => recipe.title.toUpperCase().trim().includes(value.toUpperCase().trim()));
       setRecipeList(result);
       return;
     }
-    // const ingredient = searchParams.get('ingredient');
-    // const ing = ingredients.find(el => el.name?.toUpperCase() === ingredient?.toUpperCase().trim());
-    // const result = recipes.filter(recipe => recipe.ingredients.find(el => el.id === _id.$oid));
-    // setRecipeList(result);
-    // console.log(ingredients);
+    const ingredientValue = searchParams.get('ingredient');
+    if (ingredientValue) {
+      const ingredient = ingredients.find(el => el.name?.toUpperCase() === ingredientValue?.toUpperCase().trim());
+      if (!ingredient) {
+        return;
+      }
+      const result = recipes.filter(recipe => recipe.ingredients.find(el => el.id === ingredient._id.$oid));
+      setRecipeList(result);
+      return;
+    }
   }, [searchParams]);
   return (
-    <>
+    <ListContainer>
       {recipeList.length > 0 ? (
         recipeList.map(({ _id: { $oid: id }, title, thumb }) => {
           return <CategoryRecipeCard key={id} itemId={id} imageUrl={thumb} imageAlt={title} title={title}></CategoryRecipeCard>;
@@ -38,7 +42,7 @@ const SearchedRecipesList = () => {
           <p>Try looking for something else...</p>
         </NoResultWrapper>
       )}
-    </>
+    </ListContainer>
   );
 };
 
