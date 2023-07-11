4b68231c680f018048f1f7f46046454a9fc1bcdd
OlenaHras <alena.hras@gmail.com>
2023-07-11 21:30:59 +0300

    added styles and changed favicon

--- src/components/MainPageTitle/MainPageTitle.jsx
+++ src/components/MainPageTitle/MainPageTitle.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { HeadText } from './MainPageTitle.styled';
 
 function MainPageTitle({ title }) {
-  return (
-    <HeadText>
-      {title}
-      <span className="first-square"></span>
-      <span className="second-square"></span>
-      <span className="third-square"></span>
-    </HeadText>
-  );
+    return (
+        <HeadText>
+            {title}
+            <span className="first-square"></span>
+            <span className="second-square"></span>
+            <span className="third-square"></span>
+        </HeadText>
+    );
 }
 
 export default MainPageTitle;
