0661e0b9ca819464850a2aef8a5dbb34a52614f9
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-07-11 14:52:02 +0300

    UPD: styles

--- src/pages/ShoppingListPage/ShoppingListPage.jsx
+++ src/pages/ShoppingListPage/ShoppingListPage.jsx
@@ -21,21 +21,22 @@
 
 import HeadContainer from 'components/HeadContainer/HeadContainer';
 import IngredientsShoppingList from 'components/IngredientsShoppingList/IngredientsShoppingList';
-// import ThemeSwitcher from 'services/switcherTheme';
+import MainPageTitle from 'components/MainPageTitle/MainPageTitle';
+
 import { SLContainer, SLHeader, SLHeaderText } from './ShoppingListPage.styled';
+
+const mainPageTitle = 'Shopping list';
 
 function ShoppingListPage() {
   return (
     <HeadContainer>
       <SLContainer>
-        {/* <ThemeSwitcher /> */}
-        {/* <SLMainTitle>Shopping list</SLMainTitle> */}
+        <MainPageTitle title={mainPageTitle} />
         <SLHeader>
           <SLHeaderText>Products</SLHeaderText>
           <SLHeaderText>Amount</SLHeaderText>
           <SLHeaderText>Remove</SLHeaderText>
         </SLHeader>
-
         <IngredientsShoppingList />
       </SLContainer>
     </HeadContainer>
