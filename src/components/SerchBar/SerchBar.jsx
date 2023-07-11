087a378a9011fd25c5d0f3ec4740d6c61dd19d5d
OlenaHras <alena.hras@gmail.com>
2023-07-11 18:43:52 +0300

    add styles for central position

--- src/components/SerchBar/SerchBar.jsx
+++ src/components/SerchBar/SerchBar.jsx
@@ -2,32 +2,33 @@ import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 import SearchTypeSelector from 'components/SearchTypeSelector/SearchTypeSelector';
 import Search from 'components/Search/Search';
+import { SearchBarWrapper } from './SerchBar.styled';
 
 const SearchBar = props => {
-  const [state, setState] = useState('query');
-  const navigate = useNavigate();
+    const [state, setState] = useState('query');
+    const navigate = useNavigate();
 
-  const handleQueryType = type => {
-    setState(type);
-  };
+    const handleQueryType = type => {
+        setState(type);
+    };
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    const searchQuery = e.target.elements.query.value.trim();
-    if (!searchQuery) {
-      console.log('Search query should not be empty');
-      return;
-    }
-    navigate({
-      search: `?${state}=${searchQuery}`,
-    });
-  };
-  return (
-    <>
-      <Search buttonStyle="inversion-button" handleSubmit={handleSubmit} />
-      <SearchTypeSelector setValue={handleQueryType} />
-    </>
-  );
+    const handleSubmit = e => {
+        e.preventDefault();
+        const searchQuery = e.target.elements.query.value.trim();
+        if (!searchQuery) {
+            console.log('Search query should not be empty');
+            return;
+        }
+        navigate({
+            search: `?${state}=${searchQuery}`,
+        });
+    };
+    return (
+        <SearchBarWrapper>
+            <Search buttonStyle="inversion-button" handleSubmit={handleSubmit} />
+            <SearchTypeSelector setValue={handleQueryType} />
+        </SearchBarWrapper>
+    );
 };
 
 export default SearchBar;
