bb6ef09f49d7088c8a1fa7985f42b045dada5eb2
Serhii Sydorenko <sidorchik91@gmail.com>
2023-07-12 15:27:39 +0300

    Adding Redux logic for current user and refreshing state

--- src/components/Auth/AuthForm/AuthForm.jsx
+++ src/components/Auth/AuthForm/AuthForm.jsx
@@ -7,55 +7,55 @@ import { loginUser, signupUser, logoutUser } from 'redux/auth/authOperations';
 // import { useAuth } from 'utils/hooks/useAuth';
 
 export const AuthForm = () => {
-    const { pathname } = useLocation();
-    const navigate = useNavigate();
-    const dispatch = useDispatch();
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-    const handleNavigate = useCallback(() => {
-        pathname === '/register' ? navigate('/signin') : navigate('/register');
-    }, [navigate, pathname]);
+  const handleNavigate = useCallback(() => {
+    pathname === '/register' ? navigate('/signin') : navigate('/register');
+  }, [navigate, pathname]);
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        const form = e.currentTarget;
-        if (pathname === '/signin') {
-            dispatch(
-                loginUser({
-                    email: form.elements.email.value,
-                    password: form.elements.password.value,
-                })
-            );
-            return;
-        }
-        dispatch(
-            signupUser({
-                name: form.elements.name.value,
-                email: form.elements.email.value,
-                password: form.elements.password.value,
-            })
-        );
-    };
-  
-  const handleLogout = (e) => {
-     e.preventDefault();
-    dispatch(logoutUser());
-  }
-
-    return (
-        <FormContainer>
-            <Container>
-                <Header>{pathname === '/register' ? 'Registration' : 'Sign In'}</Header>
-                <Form onSubmit={handleSubmit}>
-                    {pathname === '/register' && <Input name="name" placeholder="Name" />}
-                    <Input name="email" placeholder="Email" />
-                    <Input name="password" placeholder="Password" />
-                    <SubmitButton type="submit">{pathname === '/register' ? 'Sign up' : 'Sign In'}</SubmitButton>
-                </Form>
-                <button style={{ marginTop: 30 }} type="button" onClick={handleLogout}>
-                    Logout
-                </button>
-            </Container>
-            <Link onClick={handleNavigate}>{pathname === '/register' ? 'Sign In' : 'Registration'}</Link>
-        </FormContainer>
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    if (pathname === '/signin') {
+      dispatch(
+        loginUser({
+          email: form.elements.email.value,
+          password: form.elements.password.value,
+        })
+      );
+      return;
+    }
+    dispatch(
+      signupUser({
+        name: form.elements.name.value,
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
     );
+  };
+
+  const handleLogout = e => {
+    e.preventDefault();
+    dispatch(logoutUser());
+  };
+
+  return (
+    <FormContainer>
+      <Container>
+        <Header>{pathname === '/register' ? 'Registration' : 'Sign In'}</Header>
+        <Form onSubmit={handleSubmit}>
+          {pathname === '/register' && <Input name="name" placeholder="Name" />}
+          <Input name="email" placeholder="Email" />
+          <Input name="password" placeholder="Password" />
+          <SubmitButton type="submit">{pathname === '/register' ? 'Sign up' : 'Sign In'}</SubmitButton>
+        </Form>
+        <button style={{ marginTop: 30 }} type="button" onClick={handleLogout}>
+          Logout
+        </button>
+      </Container>
+      <Link onClick={handleNavigate}>{pathname === '/register' ? 'Sign In' : 'Registration'}</Link>
+    </FormContainer>
+  );
 };
--- src/utils/hooks/useAuth.js
+++ src/utils/hooks/useAuth.js
@@ -1,27 +1,27 @@
 import { useSelector } from 'react-redux';
 import {
-    getIsLoggedIn,
-    getUserName,
-    getUserToken,
-    getIsRefreshing,
-    getIsLoginFailed,
-    getUserAvatar
+  getIsLoggedIn,
+  getUserName,
+  getUserToken,
+  getIsRefreshing,
+  getIsLoginFailed,
+  getUserAvatar,
 } from 'redux/auth/authSelectors';
 
 export const useAuth = () => {
-    const isLoggedIn = useSelector(getIsLoggedIn);
-    const isRefreshing = useSelector(getIsRefreshing);
-    const userToken = useSelector(getUserToken);
-    const userName = useSelector(getUserName);
-    const isLoginFailed = useSelector(getIsLoginFailed);
-    const userAvatar = useSelector(getUserAvatar);
+  const isLoggedIn = useSelector(getIsLoggedIn);
+  const isRefreshing = useSelector(getIsRefreshing);
+  const userToken = useSelector(getUserToken);
+  const userName = useSelector(getUserName);
+  const isLoginFailed = useSelector(getIsLoginFailed);
+  const userAvatar = useSelector(getUserAvatar);
 
-    return {
-        isLoggedIn,
-        userToken,
-        userName,
-        isRefreshing,
-        isLoginFailed,
-        userAvatar,
-    };
+  return {
+    isLoggedIn,
+    userToken,
+    userName,
+    isRefreshing,
+    isLoginFailed,
+    userAvatar,
+  };
 };
