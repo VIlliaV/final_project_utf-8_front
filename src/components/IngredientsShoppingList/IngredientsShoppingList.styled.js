b4e04ae991760c19989e968d46fc67808e07b1e6
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-07-12 15:33:51 +0300

    prettier

--- src/components/IngredientsShoppingList/IngredientsShoppingList.styled.js
+++ src/components/IngredientsShoppingList/IngredientsShoppingList.styled.js
@@ -9,140 +9,140 @@ const tablet = '768px';
 const desktop = '1440px';
 
 const SLList = styled.ul`
-    display: flex;
-    flex-direction: column;
-    gap: 48px;
-    margin-top: 32px;
-    margin-left: 8px;
-    margin-right: 8px;
-    margin-bottom: 100px;
+  display: flex;
+  flex-direction: column;
+  gap: 48px;
+  margin-top: 32px;
+  margin-left: 8px;
+  margin-right: 8px;
+  margin-bottom: 100px;
 
-    @media screen and (min-width: ${tablet}) {
-        gap: 88px;
-        margin-top: 50px;
-        margin-left: 0;
-        margin-right: 0;
-        margin-bottom: 200px;
-    }
+  @media screen and (min-width: ${tablet}) {
+    gap: 88px;
+    margin-top: 50px;
+    margin-left: 0;
+    margin-right: 0;
+    margin-bottom: 200px;
+  }
 
-    @media screen and (min-width: ${desktop}) {
-        margin-left: 40px;
-        margin-right: 40px;
-        margin-bottom: 196px;
-    }
+  @media screen and (min-width: ${desktop}) {
+    margin-left: 40px;
+    margin-right: 40px;
+    margin-bottom: 196px;
+  }
 `;
 
 const SLItem = styled.li`
-    position: relative;
-    display: flex;
+  position: relative;
+  display: flex;
 
-    ::after {
-        content: '';
-        position: absolute;
-        left: 0;
-        bottom: -24px;
+  ::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: -24px;
 
-        width: 100%;
-        height: 1px;
-        background-color: #e0e0e0;
-    }
+    width: 100%;
+    height: 1px;
+    background-color: #e0e0e0;
+  }
 
-    @media screen and (min-width: ${tablet}) {
-        width: 704px;
-    }
+  @media screen and (min-width: ${tablet}) {
+    width: 704px;
+  }
 
-    @media screen and (min-width: ${desktop}) {
-        width: 1160px;
-    }
+  @media screen and (min-width: ${desktop}) {
+    width: 1160px;
+  }
 `;
 
 const SLItemImage = styled.div`
-    width: 60px;
-    height: 60px;
-    padding: 6px;
+  width: 60px;
+  height: 60px;
+  padding: 6px;
 
-    border-radius: 6px;
-    background: #ebf3d4;
+  border-radius: 6px;
+  background: #ebf3d4;
 
-    @media screen and (min-width: ${tablet}) {
-        width: 93px;
-        height: 97px;
-        padding: 8px 6px;
-        border-radius: 8px;
-    }
+  @media screen and (min-width: ${tablet}) {
+    width: 93px;
+    height: 97px;
+    padding: 8px 6px;
+    border-radius: 8px;
+  }
 `;
 const SLItemImageName = styled.p`
-    margin-left: 10px;
+  margin-left: 10px;
 
-    color: var(--text_second_theme_1);
-    font-family: 'Poppins', sans-serif;
-    font-size: 10px;
-    font-style: normal;
-    font-weight: 500;
-    line-height: 12px;
+  color: var(--text_second_theme_1);
+  font-family: 'Poppins', sans-serif;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 12px;
 
-    @media screen and (min-width: ${tablet}) {
-        margin-left: 16px;
+  @media screen and (min-width: ${tablet}) {
+    margin-left: 16px;
 
-        font-size: 16px;
-        line-height: 24px;
-    }
+    font-size: 16px;
+    line-height: 24px;
+  }
 `;
 
 const SlItemAmount = styled.p`
-    min-width: 38px;
-    height: 23px;
+  min-width: 38px;
+  height: 23px;
 
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-left: auto;
-    margin-right: 46px;
-    padding: 4px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: auto;
+  margin-right: 46px;
+  padding: 4px;
 
-    border-radius: 4px;
-    background: var(--active);
+  border-radius: 4px;
+  background: var(--active);
 
-    color: var(--back_theme_1);
-    text-align: center;
-    font-family: 'Poppins', sans-serif;
-    font-size: 10px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
+  color: var(--back_theme_1);
+  text-align: center;
+  font-family: 'Poppins', sans-serif;
+  font-size: 10px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
 
-    @media screen and (min-width: ${tablet}) {
-        min-width: 68px;
-        height: 35px;
+  @media screen and (min-width: ${tablet}) {
+    min-width: 68px;
+    height: 35px;
 
-        margin-right: 110px;
-        padding: 4px 8px;
+    margin-right: 110px;
+    padding: 4px 8px;
 
-        font-size: 18px;
-    }
+    font-size: 18px;
+  }
 
-    @media screen and (min-width: ${desktop}) {
-        margin-right: 174px;
-    }
+  @media screen and (min-width: ${desktop}) {
+    margin-right: 174px;
+  }
 `;
 
 const SLItemRemove = styled.button`
-    width: 14px;
-    height: 14px;
+  width: 14px;
+  height: 14px;
 
-    margin-right: 18px;
-    background-color: transparent;
-    border: none;
+  margin-right: 18px;
+  background-color: transparent;
+  border: none;
 
-    @media screen and (min-width: ${tablet}) {
-        width: 20px;
-        height: 20px;
-        margin-right: 44px;
-    }
+  @media screen and (min-width: ${tablet}) {
+    width: 20px;
+    height: 20px;
+    margin-right: 44px;
+  }
 
-    @media screen and (min-width: ${desktop}) {
-        margin-right: 28px;
-    }
+  @media screen and (min-width: ${desktop}) {
+    margin-right: 28px;
+  }
 `;
 
 export { SLList, SLItem, SLItemRemove, SlItemAmount, SLItemImageName, SLItemImage };
--- src/redux/shoppingList/shoppingListSlice.js
+++ src/redux/shoppingList/shoppingListSlice.js
@@ -1,24 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const shoppingListSlice = createSlice({
-    name: 'shoppingList',
+  name: 'shoppingList',
 
-    initialState: {
-        shoppingListIngredients: [
-            // {
-            //   _id: { $oid: '' },
-            //   name: '',
-            //   desc: '',
-            //   img: '',
-            // },
-        ],
-    },
+  initialState: {
+    shoppingListIngredients: [
+      // {
+      //   _id: { $oid: '' },
+      //   name: '',
+      //   desc: '',
+      //   img: '',
+      // },
+    ],
+  },
 
-    reducers: {
-        shoppingListGet(state, action) {
-            state.shoppingListState = action.payload.value;
-        },
+  reducers: {
+    shoppingListGet(state, action) {
+      state.shoppingListState = action.payload.value;
     },
+  },
 });
 
 export const { shoppingListGet } = shoppingListSlice.actions;
