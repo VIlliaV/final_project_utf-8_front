f1016c78bb043c6a4bffe0cf585d01ffa5597d56
VIlliaV <110853267+VIlliaV@users.noreply.github.com>
2023-07-12 21:01:48 +0000

    Deploying to gh-pages from @ VIlliaV/final_project_utf-8_front@32cdaa7d810315fc667be890d0b8e867ccafebd8 🚀

--- static/js/454.127976d5.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfinal_project_utf_8_front=self.webpackChunkfinal_project_utf_8_front||[]).push([[454],{4454:function(t,f,n){n.r(f);var u=n(8120),e=n(3329);f.default=function(){return(0,e.jsx)(u.g,{})}}}]);
-//# sourceMappingURL=454.127976d5.chunk.js.map
---
+++ static/js/898.89f41fe6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project_utf_8_front=self.webpackChunkfinal_project_utf_8_front||[]).push([[898],{4454:function(t,f,n){n.r(f);var u=n(8120),e=n(3329);f.default=function(){return(0,e.jsx)(u.g,{})}}}]);
+//# sourceMappingURL=898.89f41fe6.chunk.js.map
