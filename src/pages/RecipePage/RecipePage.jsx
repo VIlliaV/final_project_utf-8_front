3aaf7f90d593f62ac93016290b6f37e512b4b753
Iryna Popova <popovaira969@gmail.com>
2023-07-13 18:57:16 +0300

    ADD get query by id

--- src/pages/RecipePage/RecipePage.jsx
+++ src/pages/RecipePage/RecipePage.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import RecipePageHero from '../../components/RecipePageHero/RecipePageHero';
 import RecipeInngredientsList from '../../components/RecipeInngredientsList/RecipeInngredientsList';
 import RecipePreparation from '../../components/RecipePreparation/RecipePreparation';
 import { Wrapper } from './RecipePage.styled';
+import { store } from 'redux/store';
+
+const token = store.getState().auth.token;
+
+axios.defaults.baseURL = 'https://final-project-utf-8-backend.onrender.com';
+axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+
+// axois.get(`/recipes/${id}`); --отримання одного рецепта по id
+// axois.get('/ingredients/list'); --отримання списку інгрієнтів
+// axios.post("/ingredients/list",{recipeId:'id-рецепта'})
 
 function RecipePage() {
-    return (
+  const [recipe, setRecipe] = useState(null);
+  console.log(recipe);
+
+  const { recipeId } = useParams();
+  console.log(recipeId);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await axios.get(`/recipes/${recipeId}`);
+        setRecipe(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    if (recipeId) {
+      fetchRecipe();
+    }
+  }, [recipeId]);
+
+  return (
+    <div>
+      {recipe && (
         <>
-            <RecipePageHero />
-            <Wrapper>
-                <RecipeInngredientsList />
-                <RecipePreparation />
-            </Wrapper>
+          <RecipePageHero recipe={recipe} />
+          <Wrapper>
+            <RecipeInngredientsList recipe={recipe} />
+            <RecipePreparation recipe={recipe} />
+          </Wrapper>
         </>
-    );
+      )}
+    </div>
+  );
 }
 
 export default RecipePage;
