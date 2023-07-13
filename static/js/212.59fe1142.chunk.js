1641a5767fce11f73e2d947e3864b0ecee088d2e
VIlliaV <110853267+VIlliaV@users.noreply.github.com>
2023-07-13 20:46:09 +0000

    Deploying to gh-pages from @ VIlliaV/final_project_utf-8_front@b4e26e174ee91f4108e3a9b294f8eb3783d8a51d 🚀

---
+++ static/js/212.59fe1142.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project_utf_8_front=self.webpackChunkfinal_project_utf_8_front||[]).push([[212],{9212:function(t,e,f){f.r(e);var n=f(3098),u=(f(2791),f(3329));e.default=function(){return(0,u.jsx)(n.u,{page:"Favorites"})}}}]);
+//# sourceMappingURL=212.59fe1142.chunk.js.map
---
+++ static/js/848.d59085c3.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinal_project_utf_8_front=self.webpackChunkfinal_project_utf_8_front||[]).push([[848],{9848:function(e,t,f){f.r(t);var n=f(3098),u=(f(2791),f(3329));t.default=function(){return(0,u.jsx)(n.u,{page:"My recipes"})}}}]);
+//# sourceMappingURL=848.d59085c3.chunk.js.map
