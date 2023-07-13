7b96c947b3c97e0a16c5d78db9b2fed4f493f5af
MaximKarlov <predator09071987@gmail.com>
2023-07-13 14:04:36 +0300

    add fix error for teg a

--- src/components/AddRecipeForm/PopularRecipe.jsx
+++ src/components/AddRecipeForm/PopularRecipe.jsx
@@ -11,40 +11,40 @@ export default function Popular() {
       <StyledH3>Popular recipe</StyledH3>
       <ul>
         <StyledList>
-          <a href="#" alt="popular_recipe">
-            <img src={Banana_pancakes} alt="banana pincakes" />
-            <div>
-              <h4>Banana Pincakes</h4>
-              <p>In a bowl, mash the banana with a fork until it resembles a thick purée...</p>
-            </div>
-          </a>
+          {/* <a href="#" alt="popular_recipe"> */}
+          <img src={Banana_pancakes} alt="banana pincakes" />
+          <div>
+            <h4>Banana Pincakes</h4>
+            <p>In a bowl, mash the banana with a fork until it resembles a thick purée...</p>
+          </div>
+          {/* </a> */}
         </StyledList>
         <StyledList>
-          <a href="#" alt="popular_recipe">
-            <img src={Sugar_pie} alt="squash" />
-            <div>
-              <h4>Squash linguine</h4>
-              <p>Pasta is a type of food typically made from an unleavened dough of wheat flour...</p>
-            </div>
-          </a>
+          {/* <a href="#" alt="popular_recipe"> */}
+          <img src={Sugar_pie} alt="squash" />
+          <div>
+            <h4>Squash linguine</h4>
+            <p>Pasta is a type of food typically made from an unleavened dough of wheat flour...</p>
+          </div>
+          {/* </a> */}
         </StyledList>
         <StyledList>
-          <a href="#" alt="popular_recipe">
-            <img src={Backed_salmon} alt="Baked" />
-            <div>
-              <h4>Baked salmon</h4>
-              <p>Cook in boiling salted water for 10 mins...</p>
-            </div>
-          </a>
+          {/* <a href="#" alt="popular_recipe"> */}
+          <img src={Backed_salmon} alt="Baked" />
+          <div>
+            <h4>Baked salmon</h4>
+            <p>Cook in boiling salted water for 10 mins...</p>
+          </div>
+          {/* </a> */}
         </StyledList>
         <StyledList>
-          <a href="#" alt="popular_recipe">
-            <img src={Squash_linguine} alt="Sugar" />
-            <div>
-              <h4>Sugar Pie</h4>
-              <p>Sugar pie is a dessert in northern French and Belgiancuisine, where it is called tarte...</p>
-            </div>
-          </a>
+          {/* <a href="#" alt="popular_recipe"> */}
+          <img src={Squash_linguine} alt="Sugar" />
+          <div>
+            <h4>Sugar Pie</h4>
+            <p>Sugar pie is a dessert in northern French and Belgiancuisine, where it is called tarte...</p>
+          </div>
+          {/* </a> */}
         </StyledList>
       </ul>
     </StyledPopularRecipe>
