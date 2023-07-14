e4e5fe0bd24e85a3f5a77d77a9171b5189058de5
Viktoriia Bieltsova <vikav0206@gmail.com>
2023-07-14 13:41:33 +0300

    add media rules

--- src/utils/media.js
+++ src/utils/media.js
@@ -1,16 +1,17 @@
 const size = {
-    mini: '374px',
-    mobile: '375px',
-    tablet: '768px',
-    desktop: '1440px',
+  mini: '374px',
+  mobile: '375px',
+  tablet: '768px',
+  desktop: '1440px',
 };
 
 export const media = {
-    mini: `screen and (max-width: ${size.mini})`,
-    mobile: `screen and (min-width: ${size.mobile})`,
-    tablet: `screen and (min-width: ${size.tablet})`,
-    desktop: `screen and (min-width: ${size.desktop})`,
-    mobTab: `screen and (min-width: ${size.mobile}) and (max-width: ${size.tablet - 1})`,
-    tabDesk: `screen and (min-width: ${size.tablet}) and (max-width: ${size.desktop - 1})`,
-    retina: `and (-webkit-min-device-pixel-ratio: 2) `, //   tabDesk: `screen and (min-width: ${size.tablet}) and (max-width: ${size.desktop})`,
+  mini: `screen and (max-width: ${size.mini})`,
+  mobile: `screen and (min-width: ${size.mobile})`,
+  tablet: `screen and (min-width: ${size.tablet})`,
+  desktop: `screen and (min-width: ${size.desktop})`,
+  mobMax: `screen and (max-width: 767px)`,
+  mobTab: `screen and (min-width: ${size.mobile}) and (max-width: 767px)`,
+  tabDesk: `screen and (min-width: ${size.tablet}) and (max-width: 1439px)`,
+  retina: `and (-webkit-min-device-pixel-ratio: 2) `, //   tabDesk: `screen and (min-width: ${size.tablet}) and (max-width: ${size.desktop})`,
 };
