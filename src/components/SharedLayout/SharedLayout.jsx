fdb9ea4a3f03a7ba6343fa600c6cae0f68f54582
Illia <illia.vasiliev@gmail.com>
2023-07-19 21:31:33 +0300

    done fix footer

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { useAuth } from 'utils/hooks/useAuth';
 
-import { Container } from './SharedLayout.styled';
+import { Container, EmptyContent } from './SharedLayout.styled';
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
 import Loader from 'components/Loader/Loader';
@@ -13,7 +13,13 @@ export const SharedLayout = () => {
     <Container>
       {isLoggedIn && <Header />}
       <main>
-        <Suspense fallback={<Loader />}>
+        <Suspense
+          fallback={
+            <EmptyContent>
+              <Loader />
+            </EmptyContent>
+          }
+        >
           <Outlet />
         </Suspense>
       </main>
